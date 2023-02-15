3f4db91d2c5e462637bf18dd6476c81dad009590
serenecarp <122426549+serenecarp@users.noreply.github.com>
2023-02-15 15:28:56 +0900

    [D1] Title: 최대수 구하기, Time: 137 ms, Memory: 20,356 KB -BaekjoonHub

--- SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
+++ SWEA/D1/2068. 최대수 구하기/최대수 구하기.java
@@ -1,4 +1,28 @@
-k = int(input())
-for i in range (1, k+1):
-  data = list(map(int, input().split()))
-  print('#%d' %i, max(data))
+
+import java.util.Scanner;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int[] arr = new int[10];
+			int maxVal = Integer.MIN_VALUE;
+
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
+				if (maxVal < arr[i]) {
+					maxVal = arr[i];
+				}
+			}
+
+			System.out.printf("#%d %d\n", test_case, maxVal);
+
+		}
+		sc.close();
+	}
+
+}
