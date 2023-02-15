ef8be3c0297da33851ae2bcd84c9bd2209eb99a0
serenecarp <122426549+serenecarp@users.noreply.github.com>
2023-02-15 15:38:28 +0900

    [D1] Title: 중간값 찾기, Time: 135 ms, Memory: 20,644 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.java
@@ -1,4 +1,34 @@
-k = int(input())
-data = list(map(int, input().split()))
-data.sort()
-print(data[k//2])
+
+import java.util.Scanner;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < N - 1; i++) {
+			int minIdx = i;
+			for (int j = i + 1; j < N; j++) {
+				if (arr[minIdx] > arr[j]) {
+					minIdx = j;
+				}
+			}
+			swap(arr, i, minIdx);
+		}
+		System.out.println(arr[N / 2]);
+	}
+
+	// 배열 자리 바꾸는 메소드
+	// 배열은 주소값을 주므로 파라미터로 받아도 잘 바뀜
+	public static void swap(int[] arr, int idx1, int idx2) {
+		int temp = arr[idx1];
+		arr[idx1] = arr[idx2];
+		arr[idx2] = temp;
+	}
+}
