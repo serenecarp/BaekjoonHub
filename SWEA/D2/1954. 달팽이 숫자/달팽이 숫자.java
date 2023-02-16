bd6f3c274f9df2d7ff8ec7a7fd751b5459139d6c
serenecarp <122426549+serenecarp@users.noreply.github.com>
2023-02-16 15:44:01 +0900

    [D2] Title: 달팽이 숫자, Time: 142 ms, Memory: 20,964 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 class Solution {
 	public static void main(String args[]) throws Exception {
 
-		// System.setIn(new FileInputStream("res/input.txt"));
-
+		// 1. 입력할 숫자는 계속 하나씩 늘려준다
+		// 2. 아직 입력하지 않아서 0인 곳에 숫자를 남겨놓고 다음 장소로 이동한다
+		// 3. 가서는 안 될 곳에 들어가면
+		//   - 사과하고 다시 나온다.
+		//   - 델타를 이용해 방향을 바꾼다.
+		// 4. 완성되면 그대로 출력한다.
 		Scanner sc = new Scanner(System.in);
 		int T;
 		T = sc.nextInt();
@@ -16,7 +20,7 @@ class Solution {
 			int[][] snail = new int[N][N];
 			// 방향전환용 델타
 			// 오른쪽 / 아래 / 왼쪽 / 위 반복 예정
-			int[][] direction = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+			int[][] basis = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
 
 			// 번호 찍을 좌표 [i][j], 델타 제어용 k
 			int i = 0;
@@ -27,25 +31,25 @@ class Solution {
 			for (int num = 1; num <= N * N; num++) {
 				snail[i][j] = num;
 				// 현재 방향대로 전진
-				i += direction[k][0];
-				j += direction[k][1];
+				i += basis[k][0];
+				j += basis[k][1];
 				// 전진했는데 배열 범위를 넘었다면
 				// 후진했다가 방향전환
 				if (i >= N || i < 0 || j >= N || j < 0) {
-					i -= direction[k][0];
-					j -= direction[k][1];
+					i -= basis[k][0];
+					j -= basis[k][1];
 					k = ++k % 4;
-					i += direction[k][0];
-					j += direction[k][1];
+					i += basis[k][0];
+					j += basis[k][1];
 				}
 				// 전진했는데 이미 갔던 곳이라면
 				// 후진했다가 방향젼환
 				else if (snail[i][j] != 0) {
-					i -= direction[k][0];
-					j -= direction[k][1];
+					i -= basis[k][0];
+					j -= basis[k][1];
 					k = ++k % 4;
-					i += direction[k][0];
-					j += direction[k][1];
+					i += basis[k][0];
+					j += basis[k][1];
 				}
 			}
 			// 배열 그대로 출력
