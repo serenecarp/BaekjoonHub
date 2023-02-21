74fa85a483493f540d7058eb9646d6d2a3ce72c0
serenecarp <spmjoon516@gmail.com>
2023-02-21 17:56:03 +0900

    hanoi

--- 00_self_upload/Baekjoon/src/prob011729/Main.java
+++ 00_self_upload/Baekjoon/src/prob011729/Main.java
@@ -1,7 +1,23 @@
 package prob011729;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int disk = sc.nextInt();
+		int location = 1;
+		System.out.println(hanoi(disk, location));
+	}
+
+	static int hanoi(int disk, int location) {
+		int locA = (location + 1) % 3;
+		int locB = (location + 2) % 3;
+		if (disk == 1) {
+			System.out.printf("%d %d\n", disk, 3);
+			return 1;
+		}
+
+		return hanoi(disk - 1, locA) + hanoi(disk - 1, locB);
 	}
 }
