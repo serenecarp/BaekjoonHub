f3075360159538866f2580dfb6bf3fd6d251ae21
serenecarp <122426549+serenecarp@users.noreply.github.com>
2023-02-24 11:52:57 +0900

    [D3] Title: 정곤이의 단조 증가하는 수, Time: 421 ms, Memory: 56,052 KB -BaekjoonHub

---
+++ SWEA/D3/6190. 정곤이의 단조 증가하는 수/정곤이의 단조 증가하는 수.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T = sc.nextInt();
+		// 테스트케이스
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = sc.nextInt();
+			int[] seq = new int[N];
+			for (int i = 0; i < N; i++) {
+				seq[i] = sc.nextInt();
+			}
+			// 없는 경우 기본값 -1
+			int maxMonoIncr = -1;
+			// 완전탐색으로 단조증가수가 존재하는지 찾기
+			for (int i = 0; i < N - 1; i++) {
+				for (int j = i + 1; j < N; j++) {
+					int multResult = seq[i] * seq[j];
+					if (isMonoIncr(multResult)) {
+						// 최댓값이면 갱신
+						if (maxMonoIncr < multResult) {
+							maxMonoIncr = multResult;
+						}
+					}
+
+				}
+			}
+			// 출력
+			System.out.printf("#%d %d\n", test_case, maxMonoIncr);
+		}
+	}
+	// 단조증가하는 수인지 검사하는 메소드
+	static boolean isMonoIncr(int n) {
+		boolean yes = true;
+		while (n != 0) {
+			// 맨 끝자리와 그 앞자리를 비교
+			// 맨 끝자리가 더 작으면 아닌 것으로 판명 -> false 반환
+			int digit = n % 10;
+			n /= 10;
+			if (digit < n % 10) {
+				yes = false;
+				break;
+			}
+		}
+		return yes;
+	}
+}
