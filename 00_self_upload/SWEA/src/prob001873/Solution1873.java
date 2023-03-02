5cdd565c881d1a39183abb5e077dfc82b0fbab5e
serenecarp <spmjoon516@gmail.com>
2023-03-02 16:58:46 +0900

    hi

--- 00_self_upload/SWEA/src/prob001873/Solution1873.java
+++ 00_self_upload/SWEA/src/prob001873/Solution1873.java
@@ -1,10 +1,22 @@
 package prob001873;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution1873 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		int T = sc.nextInt();
+		// 테스트케이스
+		for (int testCase = 1; testCase <= T; testCase++) {
+			int H = sc.nextInt();
+			int W = sc.nextInt();
+			String[] field = new String[H];
+			for(int i=0; i<H; i++) {
+				field[i] = sc.next();
+			}
+			System.out.println(Arrays.toString(field));
+			
+		}
 	}
 }
