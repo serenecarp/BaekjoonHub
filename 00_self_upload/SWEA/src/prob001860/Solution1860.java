d1806cdc2d78f96cc66ec87c5a10f4fd46115db9
serenecarp <spmjoon516@gmail.com>
2023-03-03 11:13:29 +0900

    0303 mid

---
+++ 00_self_upload/SWEA/src/prob001860/Solution1860.java
@@ -0,0 +1,52 @@
+package prob001860;
+
+import java.util.Scanner;
+
+public class Solution1860 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		// 테스트케이스
+		for (int testCase = 1; testCase <= T; testCase++) {
+			// 손님 수, 몇 초마다, 붕어빵 몇 개 만드는지 입력
+			int customers = sc.nextInt();
+			int seconds = sc.nextInt();
+			int fishBuns = sc.nextInt();
+			// 0~11111초까지 붕어빵 개수를 나타낸 타임라인
+			int[] timeline = new int[11112];
+			// 가능/불가능
+			boolean possible = true;
+
+			// 손님을 먼저 받고
+			for (int j = 0; j < customers; j++) {
+				int getBunTime = sc.nextInt();
+				timeline[getBunTime]--;
+			}
+
+			// 붕어빵을 만드는데 붕어빵 개수가 음수면 실패
+			for (int i = 0; i < timeline.length - 1; i++) {
+
+				// seconds의 배수에만 붕어빵 추가, 0일때는 제외
+				if (i >= seconds && i % seconds == 0) {
+					timeline[i] += fishBuns;
+				}
+
+				timeline[i + 1] += timeline[i];
+
+				if (timeline[i] < 0) {
+					possible = false;
+					break;
+				}
+
+			}
+
+			// 출력
+			System.out.printf("#%d ", testCase);
+			if (possible) {
+				System.out.print("Possible\n");
+			} else {
+				System.out.print("Impossible\n");
+			}
+		}
+	}
+}
--- 00_self_upload/SWEA/src/prob001873/Solution1873.java
+++ 00_self_upload/SWEA/src/prob001873/Solution1873.java
@@ -1,6 +1,5 @@
 package prob001873;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution1873 {
@@ -9,18 +8,27 @@ public class Solution1873 {
 		int T = sc.nextInt();
 		// 테스트케이스
 		for (int testCase = 1; testCase <= T; testCase++) {
+			// 배틀필드 지도의 크기
 			int H = sc.nextInt();
 			int W = sc.nextInt();
 			char[][] field = new char[H][W];
+			// 현재 전차의 위치를 나타내는 좌표
 			int idx = 0;
 			int jdx = 0;
 			// 상 우 하 좌 == 0 1 2 3
 			int[] di = { -1, 0, 1, 0 };
 			int[] dj = { 0, 1, 0, -1 };
+			// 전차의 방향 (di, dj의 index)
 			int dir = 0;
+			
+			// 배틀필드 지도의 정보 입력 받기
+			// 동시에 현재 전차의 위치와 방향 파악하기
 			for (int i = 0; i < H; i++) {
 				field[i] = sc.next().toCharArray();
 				for (int j = 0; j < W; j++) {
+					
+					// 화살표 모양이 보이면, 그 곳이 전차의 위치
+					// 화살표의 방향에 따라 전차의 현재 방향 설정
 					if (field[i][j] == '^' || field[i][j] == '>' || field[i][j] == 'v' || field[i][j] == '<') {
 						idx = i;
 						jdx = j;
@@ -43,26 +51,39 @@ public class Solution1873 {
 				}
 			}
 
+			// 전차의 동작 개수 및 동작들 입력
 			int acts = sc.nextInt();
 			String commands = sc.next();
-			for (int actNo = 0; actNo < acts; actNo++) {
+			// 전차 동작 실행
+			outLoop: for (int actNo = 0; actNo < acts; actNo++) {
 				char act = commands.charAt(actNo);
+				// nidx, njdx == 전차의 움직일 새로운 좌표 or 전차가 쏜 포탄의 좌표
 				int nidx = idx;
 				int njdx = jdx;
+				// ********** S. 전차가 포탄을 쏜 경우 **********
 				if (act == 'S') {
-					while (nidx >= 0 && nidx < H && njdx >= 0 && njdx < W && field[nidx][njdx] != '*'
-							&& field[nidx][njdx] != '#') {
-						System.out.println(njdx);
+					// '*'이나 '#'을 만날 때까지 포탄을 전차의 현재 방향으로 전진시킨다
+					while (field[nidx][njdx] != '*' && field[nidx][njdx] != '#') {
 						nidx += di[dir];
 						njdx += dj[dir];
+						// 지도 밖으로 나갔다면 아무것도 하지 않고 다음 동작을 수행한다
+						if (nidx < 0 || nidx >= H || njdx < 0 || njdx >= W) {
+							continue outLoop;
+						}
 					}
+
+					// 벽돌 벽을 만나면 그 벽을 평지('.')로 만든다.
 					if (field[nidx][njdx] == '*') {
-						field[nidx - di[dir]][njdx - dj[dir]] = '.';
+						field[nidx][njdx] = '.';
 						continue;
-					} else
+					}
+					// 강철 벽을 만나면 아무것도 하지 않는다.
+					else
 						continue;
 				}
 
+				// ********** U, R, D, L. 전차가 움직인 경우 **********
+				// 방향 설정
 				else if (act == 'U')
 					dir = 0;
 				else if (act == 'R')
@@ -72,17 +93,26 @@ public class Solution1873 {
 				else if (act == 'L')
 					dir = 3;
 
+				// 해당 방향으로 한 칸 전진
 				nidx += di[dir];
 				njdx += dj[dir];
+				// 만약 전진한 곳이 맵 밖이거나, 갈 수 없는 곳(벽돌 벽, 강철 벽, 물)이면
+				// 아무것도 하지 않는다.
 				if (nidx < 0 || nidx >= H || njdx < 0 || njdx >= W || cannotGo(field[nidx][njdx])) {
 					continue;
-				} else {
+				}
+				// 전진한 곳이 평지라면 그 좌표로 이동한다.
+				// => 현 위치를 새 위치로 갱신한다.
+				else {
 					field[idx][jdx] = '.';
 					idx = nidx;
 					jdx = njdx;
 				}
 
 			}
+			
+			// 모든 행동을 마친 후,
+			// 전차가 있는 현위치에 현 방향에 맞는 화살표만 추가로 넣어준다.
 			switch (dir) {
 			case 0:
 				field[idx][jdx] = '^';
@@ -98,10 +128,18 @@ public class Solution1873 {
 				break;
 			}
 
-			System.out.println(Arrays.deepToString(field));
+			// 출력
+			System.out.printf("#%d ", testCase);
+			for (int i = 0; i < H; i++) {
+				for (int j = 0; j < W; j++) {
+					System.out.print(field[i][j]);
+				}
+				System.out.println();
+			}
 		}
 	}
 
+	// 전차가 갈 수 없는 곳인지 판별하는 메소드
 	static boolean cannotGo(char point) {
 		if (point == '*' || point == '#' || point == '-')
 			return true;
