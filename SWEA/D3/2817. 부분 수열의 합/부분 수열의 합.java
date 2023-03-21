2c95dd035a7d737100213ed9a8c9a30f2af3b6b4
serenecarp <122426549+serenecarp@users.noreply.github.com>
2023-03-21 11:42:14 +0900

    [D3] Title: 부분 수열의 합, Time: 2,385 ms, Memory: 23,660 KB -BaekjoonHub

---
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		// 테스트케이스
+		for (int testCase = 1; testCase <= T; testCase++) {
+			// 수열 원소 개수, 목표 합 설정
+			int N = sc.nextInt();
+			int subSum = sc.nextInt();
+			// 부분수열의 합이 subSum이 되는 경우의 수 count
+			int count = 0;
+
+			// 전체 수열 입력받기
+			int[] seq = new int[N];
+			for (int i = 0; i < N; i++) {
+				seq[i] = sc.nextInt();
+			}
+
+			// 1<<N(==2^N)가지 경우의 수를 모두 따져준다.
+			for (int i = 0; i < (1 << N); i++) {
+				// 수열의 해당 원소가 존재하면,
+				// mySum에 그 원소를 더해준다.
+				// 다 더해주면 그 경우의 수의 총합이다.
+				int mySum = 0;
+				for (int j = 0; j < N; j++) {
+					if ((i & (1 << j)) != 0) {
+						mySum += seq[j];
+					}
+				}
+				// 그 합이 목표 합과 같으면 ok
+				if (mySum == subSum) {
+					count++;
+				}
+			}
+			System.out.printf("#%d %d\n", testCase, count);
+		} // tc
+	}
+}
