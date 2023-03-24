92a6f1bb8ecee1d7cc2a9f5dcec66664cc6dde6f
serenecarp <122426549+serenecarp@users.noreply.github.com>
2023-03-24 22:23:21 +0900

    [Silver V] Title: 단어 정렬, Time: 900 ms, Memory: 40332 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,35 @@
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Map<Integer, TreeSet> wordByLen = new HashMap<>();
+
+		int N = sc.nextInt();
+		for (int i = 0; i < N; i++) {
+			String word = sc.next();
+			if (!wordByLen.containsKey(word.length())) {
+				TreeSet<String> words = new TreeSet<>();
+				words.add(word);
+				wordByLen.put(word.length(), words);
+			} else {
+				wordByLen.get(word.length()).add(word);
+			}
+		}
+
+		Map<Integer, TreeSet> sortedWordByLen = new TreeMap<>(wordByLen);
+
+		for (Integer key : sortedWordByLen.keySet()) {
+			for (Object str : sortedWordByLen.get(key)) {
+				System.out.println((String) str);
+			}
+
+		}
+
+	}
+}
