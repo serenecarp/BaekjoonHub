13225446e8c313a9aeae5c2cf0ab58c2e95d9979
serenecarp <122426549+serenecarp@users.noreply.github.com>
2023-03-25 00:06:15 +0900

    [Bronze I] Title: 팰린드롬수, Time: 196 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		boolean flag = true;
+		while (flag) {
+			char[] pelin = sc.next().toCharArray();
+			if(pelin[0] == '0') {
+				break;
+			}
+			String answer = "yes";
+			for (int i = 0; i < pelin.length / 2; i++) {
+				if (pelin[i] != pelin[pelin.length - 1 - i]) {
+					answer = "no";
+					break;
+				}
+			}
+			System.out.println(answer);
+		}
+	}
+}
