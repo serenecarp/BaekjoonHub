911ee219ac0a1d74026cf912d067111050ce86cd
serenecarp <122426549+serenecarp@users.noreply.github.com>
2023-03-25 00:17:01 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 328 ms, Memory: 32104 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		Queue<Integer> queue = new LinkedList<>();
+		for (int i = 1; i <= N; i++) {
+			queue.add(i);
+		}
+		List<Integer> answer = new ArrayList<>();
+		while (!queue.isEmpty()) {
+			for (int i = 0; i < K; i++) {
+				int tmp = queue.poll();
+				if (i == K - 1) {
+					answer.add(tmp);
+					break;
+				}
+				queue.offer(tmp);
+			}
+		}
+		System.out.print('<');
+		for (int i = 0; i < answer.size(); i++) {
+			if (i == answer.size() - 1) {
+				System.out.print(answer.get(i) + ">");
+				break;
+			}
+			System.out.print(answer.get(i) + ", ");
+		}
+	}
+}
