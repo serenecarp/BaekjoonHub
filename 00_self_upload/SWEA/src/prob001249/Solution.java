75616014fef42c2a11391eedad4733884c01591f
serenecarp <spmjoon516@gmail.com>
2023-03-30 17:53:24 +0900

    supply road unsolved

--- 00_self_upload/SWEA/src/prob001249/Solution.java
+++ 00_self_upload/SWEA/src/prob001249/Solution.java
@@ -1,5 +1,6 @@
 package prob001249;
 
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -8,6 +9,7 @@ public class Solution {
 	static int N;
 	static int[][] field;
 	static int[][] timeTaken;
+	static boolean[][] visited;
 	static PriorityQueue<Road> way;
 
 	public static void main(String[] args) {
@@ -18,6 +20,7 @@ public class Solution {
 			N = sc.nextInt();
 			// 인접행렬
 			field = new int[N][N];
+			visited = new boolean[N][N];
 
 			// 인접행렬 정보 입력
 			for (int i = 0; i < N; i++) {
@@ -27,6 +30,11 @@ public class Solution {
 				}
 			}
 
+			// 해당 좌표까지 도달하는 데 걸리는 시간, 무한대로 초기화
+			for (int i = 0; i < N; i++) {
+				Arrays.fill(timeTaken[i], INF);
+			}
+
 			Road start = new Road(0, 0, field[0][0]);
 			dijkstra(start);
 
@@ -34,29 +42,38 @@ public class Solution {
 	}
 
 	static void dijkstra(Road start) {
-		boolean[][] visited = new boolean[N][N];
-		visited[0][0] = true;
 
+		visited[start.i][start.j] = true;
+		timeTaken[start.i][start.j] = 0;
+
+		addPq(start);
+		Road currRoad = start;
+
+		while (true) {
+			Road nextRoad = way.poll();
+			visited[nextRoad.i][nextRoad.j] = true;
+			timeTaken[nextRoad.i][nextRoad.j] = timeTaken[currRoad.i][currRoad.j] + nextRoad.depth;
+			currRoad = nextRoad;
+		}
+
+	}
+
+	static void addPq(Road currRoad) {
 		int[] di = { 0, 1, 0, -1 };
 		int[] dj = { 1, 0, -1, 0 };
+		for (int dir = 0; dir < 4; dir++) {
+			int ni = currRoad.i + di[dir];
+			int nj = currRoad.j + dj[dir];
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				// 4방 델타
-				for (int dir = 0; dir < 4; dir++) {
-					int ni = i + di[dir];
-					int nj = j + dj[dir];
-					Road road = new Road(ni, nj, field[ni][nj]);
-
-					if (ni >= N || nj >= N || ni < 0 || nj < 0) {
-						continue;
-					}
-					way.add(road);
-
-				}
+			if (ni >= N || nj >= N || ni < 0 || nj < 0 || visited[ni][nj]) {
+				continue;
 			}
+			Road newRoad = new Road(ni, nj, field[ni][nj]);
+			way.add(newRoad);
+
 		}
 	}
+
 }
 
 class Road implements Comparable<Road> {
