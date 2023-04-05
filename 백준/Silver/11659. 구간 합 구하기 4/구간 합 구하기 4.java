3ab7c2079ed6d37d48c55e7cee3806f44f290b0b
Sungjun Park <122426549+serenecarp@users.noreply.github.com>
2023-04-05 14:51:49 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2444 ms, Memory: 258508 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,22 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] partialSum = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			partialSum[i] = partialSum[i - 1] + sc.nextInt();
+		}
+
+		for (int i = 0; i < M; i++) {
+			int st = sc.nextInt();
+			int ed = sc.nextInt();
+			System.out.println(partialSum[ed] - partialSum[st - 1]);
+		}
+	}
+
+}
