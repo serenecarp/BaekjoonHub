e74e174f3f678ed308b6d2eff76465d29f8a9cea
Sungjun Park <122426549+serenecarp@users.noreply.github.com>
2023-04-11 14:20:21 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 296 ms, Memory: 158044 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,57 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Subin {
+	int num;
+	int time;
+
+	public Subin(int num, int time) {
+		this.num = num;
+		this.time = time;
+	}
+}
+
+public class Main {
+	static boolean[] visited = new boolean[100001];
+	static int young, minT = Integer.MAX_VALUE;
+	static Queue<Subin> queue;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int subin = sc.nextInt();
+		young = sc.nextInt();
+		queue = new LinkedList<>();
+
+		Subin st = new Subin(subin, 0);
+		queue.offer(st);
+		BFS();
+		System.out.println(minT);
+	}
+
+	static void BFS() {
+		while (!queue.isEmpty()) {
+			Subin now = queue.poll();
+			visited[now.num] = true;
+			if (now.num == young) {
+				minT = Math.min(minT, now.time);
+			}
+
+			Subin next1 = new Subin(now.num * 2, now.time);
+			Subin next2 = new Subin(now.num + 1, now.time + 1);
+			Subin next3 = new Subin(now.num - 1, now.time + 1);
+
+			if (next1.num <= 100000 && !visited[next1.num]) {
+				queue.offer(next1);
+			}
+			if (next2.num <= 100000 && !visited[next2.num]) {
+				queue.offer(next2);
+			}
+			if (next3.num >= 0 && !visited[next3.num]) {
+				queue.offer(next3);
+			}
+
+		}
+	}
+}
