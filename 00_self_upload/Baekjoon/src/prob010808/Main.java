e6a354516de8f3a892650eee7d9f3913a7f67c41
serenecarp <spmjoon516@gmail.com>
2023-06-16 18:11:12 +0900

    a

---
+++ 00_self_upload/Baekjoon/src/prob010808/Main.java
@@ -0,0 +1,17 @@
+package prob010808;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char[] str = sc.next().toCharArray();
+		int[] countArr = new int[26];
+		for (int i = 0; i < str.length; i++) {
+			countArr[str[i] - 97]++;
+		}
+		for (int i = 0; i < countArr.length; i++) {
+			System.out.print(countArr[i] + " ");
+		}
+	}
+}
