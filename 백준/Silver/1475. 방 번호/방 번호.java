1535580702703069517306e1468a81891c504a95
Sungjun Park <122426549+serenecarp@users.noreply.github.com>
2023-06-16 18:10:25 +0900

    [Silver V] Title: 방 번호, Time: 108 ms, Memory: 12804 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char[] roomNo = sc.next().toCharArray();
+		int[] countArr = new int[10];
+		for (int i = 0; i < roomNo.length; i++) {
+			countArr[roomNo[i] - '0']++;
+		}
+		int maxCnt = 0;
+		for (int i = 0; i < 10; i++) {
+			if (i == 6 || i == 9) {
+				continue;
+			}
+			if (countArr[i] > maxCnt) {
+				maxCnt = countArr[i];
+			}
+		}
+		int tmp = (countArr[6] + countArr[9]) / 2 + (countArr[6] + countArr[9]) % 2;
+		int answer = maxCnt > tmp ? maxCnt : Math.round(tmp);
+		System.out.println((int) answer);
+	}
+}
